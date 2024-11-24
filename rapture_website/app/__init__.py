9dffbe992359d041d6852038f61a0c4fdfc66ddb
Alex Rivera <35387065+Alexriv@users.noreply.github.com>
2024-11-23 16:55:33 -0800

    ipynb to html implementation

--- rapture_website/app/__init__.py
+++ rapture_website/app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
 from flask_htmx import HTMX
 
@@ -12,9 +12,11 @@ def create_app(config_class=Config):
     app = Flask(__name__, template_folder='templates/', static_folder='static/')
     app.config.from_object(config_class)
 
+    # Initialize bcrypt for authentication
     from app.services.auth import bcrypt
     bcrypt.init_app(app)
 
+    # Initialize SocketIO and HTMX
     socketio.init_app(app)
     htmx.init_app(app)
 
@@ -35,24 +37,34 @@ def create_app(config_class=Config):
     from app.routes.admin import bp as admin_bp
     app.register_blueprint(admin_bp)
 
+    # Add context processors
     from app.context_processor import utility_processor, inject_current_app
     app.context_processor(utility_processor)
     app.context_processor(inject_current_app)
 
+    # Ensure admin user exists
     from app.services.auth import check_and_create_admin
     check_and_create_admin()
 
+    # Load nodes from the database
     from app.services.node import load_nodes_from_db
     try:
         load_nodes_from_db()
     except Exception as E:
         print(f"Failed to load nodes from db: {E}")
 
+    # Load experiment queue from the database
     from app.services.experiments import load_experiment_queue_from_db
     try:
         load_experiment_queue_from_db()
     except Exception as E:
         print(f"Failed to load experiment queue from db: {E}")
+
+    # Add a route to serve assignments from external path
+    @app.route('/assignments/<path:filename>')
+    def serve_assignments(filename):
+        assignments_directory = r"C:\Users\alexr\OneDrive\Documents\GitHub\smile-rapture\assignments"
+        return send_from_directory(assignments_directory, filename)
 
     # SocketIO imports
     from app.routes.api.node import update_node_type, exp_status_request, start_exp_press
